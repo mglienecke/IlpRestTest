eddc7f7dc1d41a60c94d95b58b8e9e3ee73186b5
Michael Glienecke <michael@glienecke.de>
2023-09-23 09:43:23 +0100

    ILP spec

--- src/main/java/uk/ac/ed/inf/OrderValidator.java
+++ src/main/java/uk/ac/ed/inf/OrderValidator.java
@@ -4,6 +4,9 @@ import uk.ac.ed.inf.ilp.data.Order;
 import uk.ac.ed.inf.ilp.data.Restaurant;
 import uk.ac.ed.inf.ilp.interfaces.OrderValidation;
 
+/**
+ * a sample order validator which does nothing
+ */
 public class OrderValidator implements OrderValidation {
     @Override
     public Order validateOrder(Order orderToValidate, Restaurant[] definedRestaurants) {
